51bd819f904c727add215a77b9e41f0113a082af
Aqvenor <119067911+141AQUA-xzy@users.noreply.github.com>
2025-03-22 19:49:41 +0530

    Update next.config.ts

--- admin/next.config.ts
+++ admin/next.config.ts
@@ -1,7 +1,14 @@
+import withPWA from "next-pwa";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig: NextConfig = withPWA({
+  reactStrictMode: true,
+  swcMinify: true,
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+});
 
 export default nextConfig;
