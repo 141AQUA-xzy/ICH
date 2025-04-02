3c4a51c879b0f574329781e912b9742069062676
141AQUA-xzy <sudeshsatpute0@gmail.com>
2025-04-02 19:13:03 +0530

    API_URI

--- admin/public/components/nav/Orders.nav.tsx
+++ admin/public/components/nav/Orders.nav.tsx
@@ -14,7 +14,7 @@ export const Orders = () => {
         showLoading()
         const fetchOrders = async () => {
             try {
-                const response = await fetch("http://192.168.43.106:5000/admin/order-book", {
+                const response = await fetch("https://ich-1gjz.onrender.com/admin/order-book", {
                     method: "GET", // ✅ Use POST if sending user details
                     headers: {
                         "Content-Type": "application/json",
